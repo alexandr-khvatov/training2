1af0be2c34fd27317ec30b24b189b34ab596ccdf
alexandr-khvatov <112820588+alexandr-khvatov@users.noreply.github.com>
2023-03-16 00:28:22 +0400

    Update HelloController.java

--- src/main/java/org/aston/api/HelloController.java
+++ src/main/java/org/aston/api/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
                 {
                     "data": {
                         "message": "Hello controller",
-                        "test-message": "Hello controller5",
+                        "test-message": "Hello controller6",
                     }
                 }
                 """;
